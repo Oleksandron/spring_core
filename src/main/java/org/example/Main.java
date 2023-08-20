c6759ee13651a41cb58e5fa0794b8fee82b425dc
Oleksandron <fedonenko.oleksandr@gmail.com>
2023-08-21 00:02:55 +0300

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,20 @@ package org.example;
 import org.example.beans.Cart;
 import org.example.beans.ProductRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Component
 public class Main {
     public static void main(String[] args) {
-        ProductRepository pr = new ProductRepository();
-        pr.addProdRep(1, "1 article", 11);
-        pr.addProdRep(2, "2 article", 22);
-        pr.addProdRep(3, "3 article", 33);
-        pr.getAllProdRep();
-        Cart one = new Cart();
-        one.ennterValue(1, "1 article", 10);
+//        ProductRepository pr = new ProductRepository();
+//        pr.addProdRep(1, "1 article", 11);
+//        pr.addProdRep(2, "2 article", 22);
+//        pr.addProdRep(3, "3 article", 33);
+//        pr.getAllProdRep();
+//        Cart one = new Cart();
+//        one.ennterValue(1, "1 article", 10);
+        ApplicationContext  ctx = new AnnotationConfigApplicationContext("org/example/beans");
+
     }
 }
